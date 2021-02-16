3dafa2a35a0f73010652ec38ff8d3ed3184ff4ff
oluwadare <wintan1418@gmail.com>
2021-02-16 13:23:54 +0100

    add codes for sessions and add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  root ‘home#index’
+  
   resources :users
-  root 'home#index'
+  resources :sessions, only: [:new, :create, :destroy]
+  get ‘signup’, to: ‘users#new’, as: ‘signup’
+  get ‘login’, to: ‘sessions#new’, as: ‘login’
+  get ‘logout’, to: ‘sessions#destroy’, as: ‘logout’
 end
